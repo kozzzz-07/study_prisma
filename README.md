ca1d54477bba74a3f955042343ca40919edf3eae
kozzzz-07 <kozzzz-07@users.noreply.github.com>
2024-10-20 02:04:53 +0900

    add cache

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 2. npx prisma init --datasource-provider sqlite
 3. npx prisma db push
 4. npx prisma studio
+
+https://www.youtube.com/watch?v=QXxy8Uv1LnQ&ab_channel=ByteGrad
--- src/app/posts/[slug]/page.tsx
+++ src/app/posts/[slug]/page.tsx
@@ -1,10 +1,29 @@
+import { unstable_cache } from "next/cache";
 import prisma from "../../../../util/db";
 
+const getCachedPost = unstable_cache((slug: string) => {
+  return prisma.post.findUnique({
+    where: {
+      slug,
+    },
+  });
+});
+
 export default async function Page({ params }: { params: { slug: string } }) {
+  // NOTE: 毎回アクセスするのではなく、キャッシュさせる`
+  // const post = await getCachedPost(params.slug);
+
+  // Accelerateを利用してキャッシュすることもできる
+  // https://www.prisma.io/docs/accelerate/getting-started#prerequisites
+  // https://www.prisma.io/docs/accelerate/caching#time-to-live-ttl
   const post = await prisma.post.findUnique({
     where: {
       slug: decodeURI(params.slug),
     },
+    // NOTE:ここに追加できる
+    // cacheStrategy: {
+    //   ttl: 60,
+    // },
   });
 
   return (
